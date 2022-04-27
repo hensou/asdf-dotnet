927fb2d3a4f2715e8f5b04296de476061b6feaa4
hensou <rickftime@gmail.com>
2022-04-27 01:46:37 +0100

    feat: Added suport for fish terminal

---
+++ set-dotnet-env.fish
@@ -0,0 +1,10 @@
+function asdf_update_dotnet_home --on-event fish_prompt
+  set --local dotnet_path (asdf which dotnet)
+  if test -n "$dotnet_path"
+    set --local full_path (realpath "$dotnet_path")
+    set -gx DOTNET_ROOT (dirname "$full_path")
+    set --local dotnet_version (dotnet --version)
+    set -gx MSBuildSDKsPath (realpath "$DOTNET_ROOT/sdk/$DOTNET_VERSION/Sdks")
+  end
+end
+
