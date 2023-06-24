39651fb3db7f867080efd5f85b0809a3ed33ba0f
hensou <rickftime@gmail.com>
2023-06-24 14:18:37 +0100

    fix: use regex instead of 'dotnet version' to speed up fish prompt

--- set-dotnet-env.fish
+++ set-dotnet-env.fish
@@ -1,11 +1,9 @@
 function asdf_update_dotnet_home --on-event fish_prompt
-  set --local dotnet_path (asdf which dotnet)
-  if test -n "$dotnet_path"
-    set --local full_path (realpath "$dotnet_path")
-    set -gx DOTNET_ROOT (dirname "$full_path")
-    set --local dotnet_version (dotnet --version)
-    set -gx MSBuildSDKsPath (realpath "$DOTNET_ROOT/sdk/$dotnet_version/Sdks")
-    
-  end
+    set --local dotnet_path (asdf which dotnet)
+    if test -n "$dotnet_path"
+        set --local full_path (realpath "$dotnet_path")
+        set -gx DOTNET_ROOT (dirname "$full_path")
+        set --local dotnet_version (string replace -r '.*/installs/dotnet/(.+)/[.].*' '$1' $dotnet_path)
+        set -gx MSBuildSDKsPath (realpath "$DOTNET_ROOT/sdk/$dotnet_version/Sdks")
+    end
 end
-
