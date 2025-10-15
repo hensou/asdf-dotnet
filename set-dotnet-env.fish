20eb1f18082ffe96bfb106cb797736082f0f9712
kotoji <kotoji@ithnote.blue>
2025-10-15 22:15:27 +0900

    fix: `set-dotnet-env.fish` (#76)
    fixes #76

--- set-dotnet-env.fish
+++ set-dotnet-env.fish
@@ -3,7 +3,7 @@ function asdf_update_dotnet_home --on-event fish_prompt
     if test -n "$dotnet_path"
         set --local full_path (realpath "$dotnet_path")
         set -gx DOTNET_ROOT (dirname "$full_path")
-        set --local dotnet_version (string replace -r '.*/installs/dotnet/(.+)/[.].*' '$1' $dotnet_path)
+        set --local dotnet_version (dotnet --version)
         set -gx MSBuildSDKsPath (realpath "$DOTNET_ROOT/sdk/$dotnet_version/Sdks")
         set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
     end
